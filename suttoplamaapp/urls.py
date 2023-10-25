5025299b67e4404b1b1a453bdd14aa522f862ced
Fatih Bektaş <fatih.bektass@gmail.com>
2023-10-25 23:12:06 +0300

    add status.html

--- suttoplamaapp/urls.py
+++ suttoplamaapp/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from suttoplamaapp import views
 
+app_name = 'suttoplamaapp'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
