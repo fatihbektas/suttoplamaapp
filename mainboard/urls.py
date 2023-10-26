5857939c764d221f6534e9298bc413abb4ea9e10
Fatih Bektaş <fatih.bektass@gmail.com>
2023-10-26 23:29:16 +0300

    add service.html

--- mainboard/urls.py
+++ mainboard/urls.py
@@ -5,8 +5,8 @@ app_name = 'mainboard'
 
 urlpatterns = [
     path("", views.mainboard, name="mainboard"),
-    path("user/", views.user_page, name="user_page"),
-    path("service/", views.service_page, name="service_page"),
+    path("user/", views.user_page, name="user-page"),
+    path("service/", views.service_page, name="service-page"),
     path("account/", views.account_settings, name="account"),
     path("customer/<str:pk>/", views.view_customer, name="customer"),
     path("chart", views.view_chart, name="chart"),
