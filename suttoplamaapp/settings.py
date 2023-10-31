5b7cf3c112585394b8ebf32b5e607ad59bb23528
Fatih Bektaş <fatih.bektass@gmail.com>
2023-10-31 14:40:04 +0300

    add login.html

--- suttoplamaapp/settings.py
+++ suttoplamaapp/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = 'images/'
 
-MEDIA_ROOT = BASE_DIR / 'static/img'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
