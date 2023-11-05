1f8d1b46aafc609b7febccb38e53458c0ef00ae6
Fatih Bektaş <fatih.bektass@gmail.com>
2023-11-06 02:20:43 +0300

    update register page

--- store/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-from mapbox_location_field.admin import MapAdmin
-
-# Register your models here.
-admin.site.register([Customer, Product, Service, OrderItem])
-admin.site.register(Order, MapAdmin)
